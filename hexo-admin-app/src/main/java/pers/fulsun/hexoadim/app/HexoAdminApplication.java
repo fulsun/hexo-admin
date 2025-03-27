d25394487c927d1e630f08870517a6d182bd5c3e
fulsun <fulsun@xx.com>
2025-03-27 18:09:01 +0800

    refactor(hexo-admin): 重构项目结构
    - 新增 hexo-admin-app 模块作为主应用
    - 移除 hexo-admin-auth 中的主应用类
    - 更新项目依赖关系
    -调整应用属性配置

---
+++ hexo-admin-app/src/main/java/pers/fulsun/hexoadim/app/HexoAdminApplication.java
@@ -0,0 +1,11 @@
+package pers.fulsun.hexoadim.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "pers.fulsun.hexoadmin")
+public class HexoAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HexoAdminApplication.class, args);
+    }
+}
